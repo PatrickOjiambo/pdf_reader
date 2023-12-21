6d6d8d870de0e6fba55b7a8ce655111930d6bed4
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-12-21 11:12:05 +0300

    Feat:Cardtile to list the documents on the screen

--- lib/widgets/document_list_tile.dart
+++ lib/widgets/document_list_tile.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
-Card documentListTile(BuildContext context, String filename, String filetype, String filesize, String filelocation, String image){
-  return Card(
-   child: Row(
-      children: <Widget>[
-        SizedBox(
-          width: 50,
-          height: 50,
-          child: Image.asset(image),
-        ),
-        Expanded(
-          flex: 1,
-          child: ListTile(
-            title: Text(filename),
-            subtitle: Text(filetype),
-            trailing: Text(filesize),
-            onTap: (){
-              Navigator.pushNamed(context, filelocation);
-            },
-          ),
-        ),
-      ],
-   ),
+import 'package:intl/intl.dart';
 
+Card documentCard(String image, DateTime date, int size, String fileName) {
+  String dateString = '${date.year}-${date.month}-${date.day}';
+  String timeString = DateFormat('h:mm a').format(date);
+  return Card(
+    child: Row(
+      children: [
+        Image(
+          image: AssetImage(image),
+          height: 50,
+          width: 50,
+        ),
+        Column(
+          children: [
+            Text(fileName),
+            Row(
+              children: [
+                Text(dateString),
+                Text(timeString),
+                Text(size as String)
+              ],
+            )
+          ],
+        ),
+        const Icon(Icons.star_border_outlined),
+        const Icon(Icons.more_vert)
+      ],
+    ),
   );
 }
