a45e7dd8171286abbb7111d39c73b40721bcb8f1
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-12-21 11:13:35 +0300

    chore: Logs to test

--- lib/utils/path_fetcher.dart
+++ lib/utils/path_fetcher.dart
@@ -5,30 +5,34 @@ import 'package:permission_handler/permission_handler.dart';
 
 // Scan for pdf files in the device and save the path in the list
 Future<List<String>> scanPdfFiles() async {
+//   debugPrint("Following is document directory");
+//  Future<void> fetchDirectories() async {
+//   Future<List<Directory>?>? externalStorageDirectories = getExternalStorageDirectories(type: StorageDirectory.documents);
+//   List<Directory>? directories = await externalStorageDirectories;
+//   debugPrint(directories.toString());
+// }
+
+// // Call the function
+// fetchDirectories();
   var permission = await Permission.manageExternalStorage.status;
   List<String> files = [];
   if (permission.isDenied) {
-    debugPrint("Permission is denied");
     await Permission.manageExternalStorage.request();
   }
   if (permission.isGranted) {
     Directory directory = Directory('/storage/emulated/0/Documents');
-    debugPrint("Reached here, permission is granted");
+
     try {
-      debugPrint("Entered try block");
-      debugPrint("the following code has an error");
       List<FileSystemEntity> entities =
           directory.listSync(recursive: true, followLinks: false);
 
       debugPrint(entities.toString());
-     files = fileFinder(entities);
+      files = fileFinder(entities);
     } catch (e) {
-      debugPrint("Error while scanning for pdf files");
       debugPrint(e.toString());
     }
     // If files list is empty then we will return empty list
     if (files.isEmpty) {
-      debugPrint("No pdf files found");
       return [];
     }
   } else if (permission.isPermanentlyDenied) {
@@ -47,13 +51,16 @@ List<String> fileFinder(List<FileSystemEntity> entities) {
       debugPrint(entity.path);
       if (entity.path.endsWith('.pdf')) {
         debugPrint("pdf file found");
+        debugPrint(entity.uri.toString());
         files.add(entity.path);
       }
     } else if (entity is Directory) {
-      debugPrint(entity.path);
-            files.addAll(fileFinder(entity.listSync(recursive: true, followLinks: false)));
-
+      continue;
+      // debugPrint(entity.path);
+      //       files.addAll(fileFinder(entity.listSync(recursive: true, followLinks: false)));
     }
   }
+  debugPrint("the following are files");
+  debugPrint(files.toString());
   return files;
 }
